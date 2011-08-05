208e7876c80c2f41b38d7a3dedc652ed0f3361b2
Dominique Rousseau <domi@lee-loo.net>
2011-08-05 22:50:23 +0200

    add fetch, config and quit commands

--- munin-relay.py
+++ munin-relay.py
@@ -40,7 +40,8 @@ class MuninClient(LineReceiver):
 
     def dataReceived(self, data):
         print "client data receive : ", data
-        self._f_parent._handle_line(self._host, data)
+        for l in (data.split("\n")):
+            self._f_parent._handle_line(self._host, l)
 
 class MuninClientFactory(ClientFactory):
     protocol = MuninClient
@@ -51,7 +52,9 @@ class MuninClientFactory(ClientFactory):
         print dir(connector)
 
 class MuninRelay(LineReceiver):
-    _re_list = re.compile("^list +(.+)$", re.I)
+    _re_list = re.compile("^list +(.+) *$", re.I)
+    _re_fetch = re.compile("^fetch +([0-9a-f]{32})_(.+) *$", re.I)
+    _re_config = re.compile("^config +([0-9a-f]{32})_(.+) *$", re.I)
     _re_comment = re.compile("^#")
     _clients = {}
     _c_factory = None
@@ -59,6 +62,22 @@ class MuninRelay(LineReceiver):
     _host2hash = {}
     _hash2host = {}
 
+    def _get_host_from_hash(self, h):
+        hostname = ''
+        if (self._hash2host.has_key(h)):
+            hostname = self._hash2host[h]
+        else:
+            for host in self.factory.cfg['hosts']:
+                print "h2h : ", host['hostname']
+                m = md5.new(host['hostname'])
+                hash_v = m.hexdigest()
+                del m
+                if (hash_v == h):
+                    hostname = host['hostname']
+                    self._hash2host[hash_v] = hostname
+                    self._host2hash[hostname] = hash_v
+        return hostname
+        
     def _open_host(self, host):
         print "_open_host:", host, self._clients
         if (self._clients.has_key(host)):
@@ -76,8 +95,11 @@ class MuninRelay(LineReceiver):
                 self._host2hash[h['hostname']] = hash_v
                 reactor.connectTCP(h['hostaddr'], int(h['port']), self._c_factory)
 
-    def _send_line(self, hostname, line):
+    def _send_line(self, hostname, line, arg=''):
         self._last_command[hostname] = line
+        if (arg != ''):
+            line = line + ' ' + arg
+        print "send line : '" + line + "' to " + hostname
         self._clients[hostname].sendLine(line)
         
     def _handle_line(self, host, line):
@@ -91,6 +113,7 @@ class MuninRelay(LineReceiver):
             return
 
         hostname = host['hostname']
+
         if (self._last_command[hostname] == 'list'):
             self._last_command[hostname] = None
             retline = ''
@@ -98,6 +121,20 @@ class MuninRelay(LineReceiver):
             for plugin in line.split(' '):
                 retline += hash_v + '_' + plugin + ' '
             self.sendLine(retline)
+            return
+            
+        if (self._last_command[hostname] == 'fetch'):
+            if (line == '.'):
+                print "end of fetch"
+                self._last_command[hostname] = None
+            self.sendLine(line)
+            return
+            
+        if (self._last_command[hostname] == 'config'):
+            if (line == '.'):
+                print "end of config"
+                self._last_command[hostname] = None
+            self.sendLine(line)
             return
             
 
@@ -121,14 +158,41 @@ class MuninRelay(LineReceiver):
             for h in self.factory.cfg['hosts']:
                 self.sendLine(h['hostname'])
             self.sendLine('.')
+        elif (data == 'quit'):
+            self.transport.loseConnection()
         else:
             m_list = self._re_list.match(data)
+            m_fetch = self._re_fetch.match(data)
+            m_config = self._re_config.match(data)
+            print m_list, m_fetch, m_config
             if (m_list != None):
                 hostname = m_list.group(1)
                 if ( not (self._clients.has_key(hostname)) ):
                     self._open_host(hostname)
                 reactor.callLater (0.1, self._send_line, hostname, 'list')
-                self.sendLine("fetch list for " + m_list.group(1))
+                self.sendLine("# fetch list for " + m_list.group(1))
+
+            if (m_fetch != None):
+                hosthash = m_fetch.group(1)
+                plugin = m_fetch.group(2)
+                print "fetch " + hosthash + " " + plugin
+                hostname = self._get_host_from_hash(hosthash)
+                if (hostname != ''):
+                    if ( not (self._clients.has_key(hostname)) ):
+                        self._open_host(hostname)
+                    reactor.callLater (0.1, self._send_line, hostname, 'fetch', plugin)
+                    self.sendLine("# fetch plugin " + plugin + " for " + hostname)
+
+            if (m_config != None):
+                hosthash = m_config.group(1)
+                plugin = m_config.group(2)
+                print "config " + hosthash + " " + plugin
+                hostname = self._get_host_from_hash(hosthash)
+                if (hostname != ''):
+                    if ( not (self._clients.has_key(hostname)) ):
+                        self._open_host(hostname)
+                    reactor.callLater (0.1, self._send_line, hostname, 'config', plugin)
+                    self.sendLine("# config plugin " + plugin + " for " + hostname)
 
 
 class MuninRelayFactory(Factory):
